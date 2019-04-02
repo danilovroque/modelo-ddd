417e10d7ca4bbec860cb09965beb21c7c542d398
Danilo Vinícius Roque <daniloviniciusroque@gmail.com>
2019-04-01 22:58:41 -0300

    Adding EntityFramework Library + Creating the Context

---
+++ ProjetoModeloDDD.Infra.Data/Context/ProjetoModeloContext.cs
@@ -0,0 +1,18 @@
+﻿using ProjetoModeloDDD.Domain.Entities;
+using System.Data.Entity;
+
+namespace ProjetoModeloDDD.Infra.Data.Context
+{
+    public class ProjetoModeloContext : DbContext
+    {
+
+        public ProjetoModeloContext() 
+            : base("ProjetoModeloDDD")
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+    }
+}
