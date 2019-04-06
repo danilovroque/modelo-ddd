dfe06e4fc21aa1032af0193a07edb020e1147a21
Danilo Vinícius Roque <daniloviniciusroque@gmail.com>
2019-04-06 18:14:25 -0300

    Configurando o DBContext e adicionando classe produtos

--- ProjetoModeloDDD.Infra.Data/Context/ProjetoModeloContext.cs
+++ ProjetoModeloDDD.Infra.Data/Context/ProjetoModeloContext.cs
@@ -1,18 +1,59 @@
 ﻿using ProjetoModeloDDD.Domain.Entities;
+using ProjetoModeloDDD.Infra.Data.EntityConfig;
+using System;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
 
 namespace ProjetoModeloDDD.Infra.Data.Context
 {
     public class ProjetoModeloContext : DbContext
     {
-
-        public ProjetoModeloContext() 
+        public ProjetoModeloContext()
             : base("ProjetoModeloDDD")
         {
 
-        }
+        }   
 
         public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
 
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>(); //Remove convenção que automaticamente coloca o nome das tabelas no plural
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>(); //Remove convenção de deletar por cascata quando tiver relações um para muitos
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            modelBuilder.Properties()
+                .Where(p => p.Name == p.ReflectedType.Name + "Id")
+                .Configure(p => p.IsKey()); //Quando houver alguma propriedade que o nome dela é a classe + Id, automaticamente essa propriedade é a chave primária
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasColumnType("varchar")); //Sempre que o campo for string, cria no banco de dados como varchar como default
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasMaxLength(200)); //Seta os campos varchar como MaxLength = 200 como default - pode ser alterado de acordo com a necessidade de cada campo 
+
+            modelBuilder.Configurations.Add(new ClienteConfiguration());
+            modelBuilder.Configurations.Add(new ProdutoConfiguration());
+        }
+
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataCadastro").IsModified = false;
+                }
+            }
+
+            return base.SaveChanges();
+        }
     }
 }
---
+++ ProjetoModeloDDD.Infra.Data/Migrations/201904062103196_Create database.cs
@@ -0,0 +1,30 @@
+namespace ProjetoModeloDDD.Infra.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Createdatabase : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Cliente",
+                c => new
+                    {
+                        ClienteId = c.Int(nullable: false, identity: true),
+                        Nome = c.String(nullable: false, maxLength: 150, unicode: false),
+                        Sobrenome = c.String(nullable: false, maxLength: 150, unicode: false),
+                        Email = c.String(nullable: false, maxLength: 200, unicode: false),
+                        DataCadastro = c.DateTime(nullable: false),
+                        Ativo = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.ClienteId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Cliente");
+        }
+    }
+}
