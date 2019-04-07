689550552c6484a1ad9f1ec6f4ec3d57c9eb5e35
Danilo Vinícius Roque <daniloviniciusroque@gmail.com>
2019-04-07 11:39:44 -0300

    Adicionando repositórios (Cliente, Produto)

---
+++ ProjetoModeloDDD.Infra.Data/Repositories/RepositoryBase.cs
@@ -0,0 +1,48 @@
+﻿using ProjetoModeloDDD.Domain.Interfaces;
+using ProjetoModeloDDD.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace ProjetoModeloDDD.Infra.Data.Repositories
+{
+    public class RepositoryBase<TEntity> : IDisposable, IRepositoryBase<TEntity> where TEntity : class
+    {
+        protected ProjetoModeloContext Db = new ProjetoModeloContext(); 
+
+        public void Add(TEntity obj)
+        {
+            Db.Set<TEntity>().Add(obj);
+            Db.SaveChanges();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            //return Db.Set<TEntity>().AsNoTracking().ToList(); - Desabilita o tracking, não fazendo o lazy load (pode econimizar recursos). Podemos criar um outro método para trazer apenas o AsNoTracking
+            return Db.Set<TEntity>().ToList();
+        }
+
+        public TEntity GetById(int id)
+        {
+            return Db.Set<TEntity>().Find(id);
+        }
+
+        public void Remove(TEntity obj)
+        {
+            Db.Set<TEntity>().Remove(obj);
+            Db.SaveChanges();
+        }
+
+        public void Update(TEntity obj)
+        {
+            Db.Entry(obj).State = EntityState.Modified;
+            Db.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
