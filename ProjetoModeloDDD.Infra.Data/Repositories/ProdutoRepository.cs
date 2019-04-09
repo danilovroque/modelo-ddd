0f61c218e6db1ce9129b764ea57eec72eb528181
Danilo Vinícius Roque <daniloviniciusroque@gmail.com>
2019-04-08 23:35:10 -0300

    Criando interfaces de serviço

--- ProjetoModeloDDD.Infra.Data/Repositories/ProdutoRepository.cs
+++ ProjetoModeloDDD.Infra.Data/Repositories/ProdutoRepository.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using ProjetoModeloDDD.Domain.Entities;
-using ProjetoModeloDDD.Domain.Interfaces;
+using ProjetoModeloDDD.Domain.Interfaces.Repositories;
 
 namespace ProjetoModeloDDD.Infra.Data.Repositories
 {
